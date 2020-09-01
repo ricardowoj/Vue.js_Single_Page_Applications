7c888f4b4ca6848143d697b523e0e34385fc438e
ricardowoj <ricardowoj@hotmail.com>
2020-09-01 18:08:23 -0300

    edit App.vue: The power of directives

--- polacopic/src/App.vue
+++ polacopic/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <div class="container middle">
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo">
+    <ul style="list-style: none;">
+      <li v-for="foto of fotos" :key="foto.id">
+        <img class="rounded-circle" :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
   </div>
 
 </template>
@@ -12,10 +17,16 @@ export default {
 
     return {
       titulo: 'PolacoPic',
-      foto: {
-        url: 'https://i.pinimg.com/originals/ce/1a/46/ce1a46ef454eb388d4dfb26c8f96ca9b.png',
-        titulo: 'Pug homens de preto'
-      }
+      fotos: [
+        {
+          url: 'https://i.pinimg.com/originals/ce/1a/46/ce1a46ef454eb388d4dfb26c8f96ca9b.png',
+          titulo: 'Pug homens de preto'
+        },
+        {
+          url: 'https://http2.mlstatic.com/D_NQ_NP_780287-MLB29606162644_032019-O.jpg',
+          titulo: 'Pug homens de preto - MIB3'
+        }
+      ]
     }
   }
 }
