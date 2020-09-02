e16face3b9506a4b21714019c5825c1806741ee3
ricardowoj <ricardowoj@hotmail.com>
2020-09-02 12:59:26 -0300

    create Painel.vue: isolating markup and styles in a new component

---
+++ polacopic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="container">
+    <!-- As a heading -->
+    <nav class="navbar bg-info text-white font-weight-bold">
+      <span class="navbar-brand mb-0 h1 mx-auto">Vue.js Pictures</span>
+    </nav>
+    <div class="row" style="list-style: none;">
+
+      <div class="col mb-5 mr-1 mt-5 rounded-lg" v-for="foto of fotos" :key="foto.id">
+
+        <div class="card mx-auto" style="width: 18rem;">
+          <img class="bd-placeholder-img card-img-top" width="100%" height="180" :src="foto.url" :alt="foto.titulo">
+
+          <div class="card-body font-weight-bold mx-auto">
+            <p class="card-text">{{ foto.titulo }}</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+</template>
