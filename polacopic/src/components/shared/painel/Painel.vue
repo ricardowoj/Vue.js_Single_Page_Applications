891db287c92e86981f09b4f2fe8d2f9beebf1804
ricardowoj <ricardowoj@hotmail.com>
2020-09-02 15:01:45 -0300

    edit Painel.vue: Importing and communicating with the component

--- polacopic/src/components/shared/painel/Painel.vue
+++ polacopic/src/components/shared/painel/Painel.vue
@@ -1,25 +1,16 @@
 <template>
 
-  <div class="container">
-    <!-- As a heading -->
-    <nav class="navbar bg-info text-white font-weight-bold">
-      <span class="navbar-brand mb-0 h1 mx-auto">Vue.js Pictures</span>
-    </nav>
-    <div class="row" style="list-style: none;">
-
-      <div class="col mb-5 mr-1 mt-5 rounded-lg" v-for="foto of fotos" :key="foto.id">
-
-        <div class="card mx-auto" style="width: 18rem;">
-          <img class="bd-placeholder-img card-img-top" width="100%" height="180" :src="foto.url" :alt="foto.titulo">
-
-          <div class="card-body font-weight-bold mx-auto">
-            <p class="card-text">{{ foto.titulo }}</p>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
+<div class="card border border-info shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
+  <slot></slot>
+  <div class="card-body font-weight-bold mx-auto">
+    <p class="card-text">{{ titulo }}</p>
   </div>
+</div>
 
 </template>
+
+<script>
+export default {
+  props: ['titulo']
+}
+</script>
